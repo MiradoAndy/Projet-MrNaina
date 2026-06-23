222f8ce2498705ffe01a39fde290834720342266
MiradoAndy <miradoandy14@gmail.com>
2026-06-23 16:50:17 +0300

    fonction pour tester

--- src/mg/itu/controllers/Test1Controller.java
+++ src/mg/itu/controllers/Test1Controller.java
@@ -1,7 +1,16 @@
 package mg.itu.controllers;
-import mg.itu.annotation.MonController;
 
-@MonController
+import mg.itu.annotation.MonController;
+import mg.itu.annotation.MaFonction;
+
+@MonController(url = "/test1")
 public class Test1Controller {
-    
+
+    @MaFonction
+    public void afficherAccueil() {}
+
+    @MaFonction
+    public void listerElements() {}
+
+    public void methodeNonExposee() {}
 }
--- src/mg/itu/controllers/Test2Controller.java
+++ src/mg/itu/controllers/Test2Controller.java
@@ -1,7 +1,13 @@
 package mg.itu.controllers;
-import mg.itu.annotation.MonController;
 
-@MonController
+import mg.itu.annotation.MonController;
+import mg.itu.annotation.MaFonction;
+
+@MonController(url = "/test2")
 public class Test2Controller {
-    
+
+    public void creerItem() {}
+
+    @MaFonction
+    public void supprimerItem() {}
 }
